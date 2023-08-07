4170433e84135202853865316e80662f34b15974
esafaa <132300437+esafaa@users.noreply.github.com>
2023-08-06 21:39:46 -0500

    creation of User entity

---
+++ schoolfieldtrips/src/main/java/org/safaaess/schoolfieldtrips/model/User.java
@@ -0,0 +1,40 @@
+package org.safaaess.schoolfieldtrips.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+/*    private String phoneNumber;
+    private String address;*/
+    private String username;
+    private String password;
+    private String role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    List<Approval> approval = new ArrayList<>();
+}
