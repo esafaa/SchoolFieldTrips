537812cdb271e253a590e35700f7e9c1ba94057f
esafaa <132300437+esafaa@users.noreply.github.com>
2023-08-06 20:58:41 -0500

    First commit

---
+++ schoolfieldtrips/src/main/java/org/safaaess/schoolfieldtrips/SchoolfieldtripsApplication.java
@@ -0,0 +1,13 @@
+package org.safaaess.schoolfieldtrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolfieldtripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolfieldtripsApplication.class, args);
+	}
+
+}
---
+++ schoolfieldtrips/src/test/java/org/safaaess/schoolfieldtrips/SchoolfieldtripsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.safaaess.schoolfieldtrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolfieldtripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
