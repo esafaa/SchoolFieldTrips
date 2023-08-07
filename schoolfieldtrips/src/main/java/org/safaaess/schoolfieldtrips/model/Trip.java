f4748e6bdec3528236584bbf919de3575fe2cd05
esafaa <132300437+esafaa@users.noreply.github.com>
2023-08-06 22:04:29 -0500

    adding comment

--- schoolfieldtrips/src/main/java/org/safaaess/schoolfieldtrips/model/Trip.java
+++ schoolfieldtrips/src/main/java/org/safaaess/schoolfieldtrips/model/Trip.java
@@ -20,7 +20,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-/*  Trip entity containig trip details , attachments file, and approvals  
+/*  Trip entity containig trip details , attachments file, and approvals
+ *  one trip can have multiple files associated with it.
+ *  one trip can have multiple approvals associated with it.  
 */
 
 @Getter
@@ -31,26 +33,26 @@ import lombok.ToString;
 @Entity
 @Table(name = "trips")
 public class Trip {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String destination;
-    private String tripLeader;
-    private String otherStaffAttending;
-    private String purpose;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date date;
-    @DateTimeFormat(pattern = "HH:mm")
-    private LocalTime departureTime;
-    @DateTimeFormat(pattern = "HH:mm")
-    private LocalTime returnTime;
-    private int numberOfStudents;
-    private double price;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String destination;
+	private String tripLeader;
+	private String otherStaffAttending;
+	private String purpose;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date date;
+	@DateTimeFormat(pattern = "HH:mm")
+	private LocalTime departureTime;
+	@DateTimeFormat(pattern = "HH:mm")
+	private LocalTime returnTime;
+	private int numberOfStudents;
+	private double price;
 
-    @OneToMany(mappedBy = "trip", cascade = CascadeType.ALL)
-    private List<File> tripFiles = new ArrayList<>();
+	@OneToMany(mappedBy = "trip", cascade = CascadeType.ALL)
+	private List<File> tripFiles = new ArrayList<>();
 
-    @OneToMany(mappedBy = "trip", cascade = CascadeType.ALL)
-    List<Approval> approval;
+	@OneToMany(mappedBy = "trip", cascade = CascadeType.ALL)
+	List<Approval> approval;
 }
